3a9324c832fc7954d0bc79e7c8823ac9aae8f3e2
Avazbek-02 <avazbekmambetov9@gmail.com>
2024-10-01 02:24:42 +0500

    struct is done

---
+++ migration/000001_insert_table.up.sql
@@ -0,0 +1,13 @@
+create type category as enum('ovqat','texnika','telefon','avtamabil','uy');
+
+create table if exists shop(
+    id uuid primary key not null,
+    name varchar(250) not null,
+    img_url varchar(300) not null,
+    caregorys category not null,
+    user_name varchar(100) not null, 
+    user_phone varchar(20) not null,
+    created_at timestamp default now() not null, 
+    updated_at timestamp default now() not null, 
+    deleted_at bigint default 0 not null
+)
